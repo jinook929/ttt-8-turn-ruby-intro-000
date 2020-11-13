6caf9b70f52478dd4bee1d94fec5e8b505d30e22
JINOOK JUNG <jinook929@gmail.com>
2020-11-13 03:40:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,12 +8,22 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+<<<<<<< HEAD
   input = gets.strip
   index = input_to_index(input)
   if(valid_move?(board, index))
     move(board, index)
     display_board(board)
     #turn(board)
+=======
+  index = gets.strip.to_i - 1
+  if index.between?(0, 8)
+    if(valid_move?(board, index))
+      move(board, index)
+      display_board(board)
+      turn(board)
+    end
+>>>>>>> de346c70225006d6d875efc04b1d33defbe24caf
   else
     turn(board)
   end
@@ -28,7 +38,11 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
+<<<<<<< HEAD
   if((board[index] == " " || board[index] == "" || board[index] == nil) && (index.between?(0, 8)))
+=======
+  if(board[index] == " " || board[index] == "" || board[index] == nil)
+>>>>>>> de346c70225006d6d875efc04b1d33defbe24caf
     return true
   else
     return false
